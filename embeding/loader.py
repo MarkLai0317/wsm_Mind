2e897a27e380ab1b0d20625562b603301a543e1d
YJack <jackleagends@gmail.com>
2021-06-18 00:03:42 +0800

    修復 embeder

--- embeding/loader.py
+++ embeding/loader.py
@@ -1,15 +1,24 @@
 import gensim
 import pandas as pd
 
-news_path = '/Users/yjack0827/Downloads/wsmMind/data/MINDlarge_train/news.tsv'
+news_path = '/Users/yjack0827/Downloads/wsmMind/data/test/news.tsv'
 
 news_df = pd.DataFrame(pd.read_csv(news_path, index_col=0, sep="\t", header=None, usecols = [i for i in range(5)]))\
           .rename_axis(index="newsID").rename(columns={1: 'Category', 2: 'SubCategory', 3: 'title', 4: 'Abstract' })
 
-model_path = ""
+model_path = "/Users/yjack0827/Downloads/wsmMind/embeding/model/doc2vec_large_test.model"
 
-model.load(model_path)
+model = gensim.models.Doc2Vec.load(model_path)
 
+#print(model.docvecs)
+
+with open('../newsinfo/large_test_news.tsv', 'w') as f:
+    for news_id in news_df.index:
+        print(news_id)
+        f.write(news_id + "\t[" + ' '.join(map(str, model.dv[news_id])) + "]\n")
+
+"""
 with open('../newsinfo/large_train_news.tsv', 'w') as f:
     for news_id, i in zip(news_df.index, range(0, news_df.index.size)):
-        f.write(news_id + '\t' + ' '.join(map(str, model.docvecs[i])) + '\n')
+        f.write(news_id + '\t' + ' '.join(map(str, model.dv[i])) + '\n')
+"""
