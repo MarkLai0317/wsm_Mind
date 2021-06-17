ed8719163477c9648bb655b2eccc3f79aeb29eed
24135768b <58065881+24135768b@users.noreply.github.com>
2021-06-18 01:13:49 +0800

    .

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ import DataParser
 import IR_functions
 import os
 
-news_info_file = '../newsinfo/news_embed.tsv'
-test_behaviors_file = '../test/behaviors.tsv'
+news_info_file = '../newsinfo/small_valid_news.tsv'
+test_behaviors_file = '../../MINDsmall_valid/small_behaviors.tsv'
 
 dp = DataParser.DataParser(news_info_file, test_behaviors_file)
 
-
+print(dp._behaviorDF)
 # In[ ]:
 
 
@@ -27,19 +27,20 @@ print("Number of Impression: ", number_of_impressions)
 
 # In[ ]:
 
+count = 1
+for user_impression_ID in range(1, number_of_impressions+1):
+  print("Process (", count,end='\r')
+  count += 1
 
-for user_impression_ID in range(1, number_of_impressions):
   user_history = dp.getHistory(user_impression_ID)
   normalized_history = IR_functions.normalizeHistory(user_history)
   
-  user_impression = dp.getImpression(user_impression_ID, data_type = 'test')
-  score = IR_functions.cosSimilarity(normalized_history, user_impression[0])
+  user_impression = dp.getImpression(user_impression_ID, data_type = 'valid')
+  score = IR_functions.cosSimilarity(normalized_history, user_impression)
 
   #truth.append(user_impression[1])
-
   answer = IR_functions.sortCandidateNews(score)
   answer_list.append(answer)
-
 
 # In[ ]:
 
@@ -47,7 +48,7 @@ for user_impression_ID in range(1, number_of_impressions):
 import numpy as np
 import os
 
-with open(os.path.join('../result', 'prediction.txt'), 'w') as f:
+with open(os.path.join('../result', 'prediction_test.txt'), 'w') as f:
   impr_index = 0
   for preds in answer_list:
       impr_index += 1
