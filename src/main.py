58f1e49ea17fb0785cd017e730f35601df4c8acf
24135768b <58065881+24135768b@users.noreply.github.com>
2021-06-18 02:42:08 +0800

    main add args

--- src/main.py
+++ src/main.py
@@ -3,10 +3,28 @@
 
 # In[ ]:
 
-
 import DataParser
 import IR_functions
 import os
+import sys
+
+if len(sys.argv) > 4 :
+	print("too many arguments")
+	sys.exit()
+
+embedding_parameter = sys.argv[1]
+feedback_list = []
+
+for i in range(2,len(sys.argv)):
+	feedback_list.append(sys.argv[i])
+
+str_feedback = [int(10*float(i)) for i in feedback_list]
+feedback = [float(i) for i in feedback_list]
+str1 = ''.join(str(e) for e in str_feedback)
+
+prediction_file_name = "prediction_" + sys.argv[1] + "_" + str1 + ".txt"
+#print(prediction_file_name)
+#print(feedback)
 
 news_info_file = '../newsinfo/small_valid_news.tsv'
 test_behaviors_file = '../../MINDsmall_valid/small_behaviors.tsv'
@@ -35,7 +53,7 @@ for user_impression_ID in range(1, number_of_impressions+1):
   normalized_history = IR_functions.normalizeHistory(user_history)
   
   user_impression = dp.getImpression(user_impression_ID, data_type = 'valid')
-  score = IR_functions.cosSimilarity(normalized_history, user_impression)
+  score = IR_functions.cosSimilarity(normalized_history, user_impression, feedback)
 
   #truth.append(user_impression[1])
   answer = IR_functions.sortCandidateNews(score)
@@ -47,7 +65,8 @@ for user_impression_ID in range(1, number_of_impressions+1):
 import numpy as np
 import os
 
-with open(os.path.join('../result', 'prediction_test.txt'), 'w') as f:
+
+with open(os.path.join('../result', prediction_file_name, 'w')) as f:
   impr_index = 0
   for preds in answer_list:
       impr_index += 1
