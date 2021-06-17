e2d20bf3d449a2994f09238650fa951dbf00ee87
24135768b <58065881+24135768b@users.noreply.github.com>
2021-06-17 20:54:21 +0800

    .

--- embeding/embeder.py
+++ embeding/embeder.py
@@ -5,7 +5,7 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 import gensim
 
-news_path = '/Users/yjack0827/Downloads/wsmMind/test/news.tsv'
+news_path = '/Users/yjack0827/Downloads/wsmMind/test/news.tsv'#####
 
 news_df = pd.DataFrame(pd.read_csv(news_path, index_col=0, sep="\t", header=None, usecols = [i for i in range(5)]))\
           .rename_axis(index="newsID").rename(columns={1: 'Category', 2: 'SubCategory', 3: 'title', 4: 'Abstract' })
@@ -38,7 +38,7 @@ for i in news_df.index:
 print('training...')
 model = gensim.models.Doc2Vec(documents, dm=1, vector_size=100, window=8, min_count=5, workers=2)
 
-model.save('model/doc2vec_large_train.model')
-with open('../newsinfo/large_test_news.tsv', 'w') as f:
+model.save('model/doc2vec_large_train.model')######
+with open('../newsinfo/large_test_news.tsv', 'w') as f:######
     for news_id, i in zip(news_df.index, range(0, news_df.index.size)):
         f.write(news_id + " " + ' '.join(map(str, model.docvecs[i])) + '\n')
